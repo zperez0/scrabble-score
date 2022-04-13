8bbf42638f9d1c125058e93f9e91aa4e92829add
Yazmin Perez <zoeperez333@gmail.com>
2022-04-12 21:13:59 -0700

    test 1 passing

---
+++ lib/scrabble_score.rb
@@ -0,0 +1,20 @@
+require('pry')
+
+class String
+
+  def scrabble_score()
+    if self == "a"
+      1
+    end
+  end
+end
+
+# Class Scrabble
+# def score(letter)
+#   points = {"A" => 1}
+# end
+
+# ("a".."z")
+# (1..10)
+
+# spilt , add each letter, push into an array?
