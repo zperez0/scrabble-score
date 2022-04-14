a61ec5831f7afda087110d7b4bd11ea58ef0aae6
Yazmin Perez <zoeperez333@gmail.com>
2022-04-14 14:15:03 -0700

    update lib. test passing 1-4. WIP

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,20 +1,64 @@
 require('pry')
 
-class String
-
-  def scrabble_score()
-    if self == "a"
-      1
+class Word
+    def initialize(word)
+      @word = word.downcase
     end
-  end
-end
+  
+    def scrabble_score()
+      points = {
+        "a" => 1, 
+        "e" => 1,
+        "o" => 1,
+        "d" => 2,
+        "g" => 2
+      }
+      score = 0
+      @word.each_char do |letter|
+        score += points[letter]
+      end
+      score
+      end
+    end
 
-# Class Scrabble
-# def score(letter)
-#   points = {"A" => 1}
+    # filter out spaces, non letter characters (reg exp)
 # end
+
+# "abcd".each_char
+# a
+# b
+  
+
+
 
 # ("a".."z")
 # (1..10)
 
-# spilt , add each letter, push into an array?
+# spilt , add each letter, push into an array, map(), myHash {"a" => 1, }
+
+
+# class String
+
+#   def scrabble_score()
+      
+#     if self ==  "a"
+#       1
+#     elsif self == 'e'
+#     1
+#     end
+
+#   end
+
+# end
+
+
+  # points = @word.downcase.split("")
+      # points.each do |points|
+      # if points = {
+      #   "a" => 1,
+      #   "e" => 1,
+      #   "i" => 1,
+      # }
+
+      # points.values_at(*).compact
+      # points.map{|x| x.values}
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,11 +1,18 @@
 require('rspec')
 require('scrabble_score')
 
-describe(String) do
+describe(Word) do
   describe('#scrabble_score') do
     it("returns a scrabble score for a letter") do
-    expect("a".scrabble_score()).to(eq(1))
-    # expect("e".scrabble_score()).to(eq(1))
+      points = Word.new("a")
+      points1 = Word.new("e")
+      points2 = Word.new("dog")
+      points3 = Word.new("Dog")
+      # points2 = Word.new("i")
+    expect(points.scrabble_score()).to(eq(1))
+    expect(points1.scrabble_score()).to(eq(1))
+    expect(points2.scrabble_score()).to(eq(5))
+    expect(points3.scrabble_score()).to(eq(5))
     end
   end
 end
